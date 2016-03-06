20d30191ad2fcca3f8b5956c26331f64de912825
urso <steffen.siering@elastic.co>
2016-03-06 02:54:43 +0100

    init

---
+++ {{cookiecutter.module}}/config.go
@@ -0,0 +1,18 @@
+package {{ cookiecutter.module }}
+
+import (
+	"github.com/elastic/beats/packetbeat/config"
+	"github.com/elastic/beats/packetbeat/protos"
+)
+
+type {{ cookiecutter.module }}Config struct {
+	config.ProtocolCommon `config:",inline"`
+}
+
+var (
+	defaultConfig = {{ cookiecutter.module }}Config{
+		ProtocolCommon: config.ProtocolCommon{
+			TransactionTimeout: protos.DefaultTransactionTimeout,
+		},
+	}
+)
